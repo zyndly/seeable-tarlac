39abf2450d0f2b0d6d512a9173fbbce5abdee779
Zyndly <zyndlykentperlinbaja@gmail.com>
2022-08-22 19:18:16 +0800

    feat: add Layout and SEO

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -6,7 +6,9 @@ import React, { useContext } from 'react';
 import useScrollDirection from '@/hooks/useScrollDirection';
 import { useWindowScrollPositions } from '@/hooks/useWindowScrollPositions';
 
+import Layout from '@/components/layout/Layout';
 import { Author, Categories, CollectionsWidget,Comments, CommentsForm, Loader, PostDetail, PostWidget } from '@/components/post'; 
+import Seo from '@/components/Seo';
 
 import { StateContext } from '@/pages/_app';
 import { getPostDetails,getPosts } from '@/services'; 
@@ -27,46 +29,50 @@ const PostDetails = ( { post } : PostDetailsProps) : JSX.Element => {
     }
 
     return (
-        <div className={'container mx-auto md:px-10 mb-8'+(menu?' blur-filter': ' trans-500')}>
+    <Layout>
+      {/* <Seo templateTitle='Home' /> */}
+      <Seo
+        templateTitle='Destinations'
+        description='Tourist Destinations in Tarlac, Province'
+      />
+            <div className={'container mx-auto md:px-10 mb-8'+(menu?' blur-filter': ' trans-500')}>
 
-            <div className='grid grid-cols-1 lg:grid-cols-5 gap-1'>
+                <div className='grid grid-cols-1 lg:grid-cols-5 gap-1'>
 
-                <div className='hidden lg:block lg:col-span-1'>
-                    <div className={'relative lg:sticky transition-all duration-300  '
-                        + (scrollDirection === 'up' || scrollY < 30 ?  ' lg:top-[100px]' : ' lg:top-[20px]')
-                    }>
-                        <CollectionsWidget />
-                        
+                    <div className='hidden lg:block lg:col-span-1'>
+                        <div className={'relative lg:sticky transition-all duration-300  '
+                            + (scrollDirection === 'up' || scrollY < 30 ?  ' lg:top-[100px]' : ' lg:top-[20px]')
+                        }>
+                            <CollectionsWidget />
+                            
+                        </div>
+                    </div>
+
+                    <div className='col-span-1 lg:col-span-3'>
+
+                        <PostDetail post={post}/>
+                        <Author author={post.author}/>
+                        <CommentsForm slug={post.slug}/>
+                        <Comments slug={post.slug}/>
 
                     </div>
-                    
-                </div>
 
-                <div className='col-span-1 lg:col-span-3'>
+                    <div className='col-span-1'>
+                        <div className={'relative lg:sticky transition-all duration-300 '
+                            + (scrollDirection === 'up' || scrollY < 30 ?  ' lg:top-[100px]' : ' lg:top-[20px]')
+                        }>
+                            <PostWidget 
+                                slug={post.slug}
+                                categories={post.categories.map((category: any) => category.slug) }
+                                />
+                            <Categories />
 
-                    <PostDetail post={post}/>
-                    <Author author={post.author}/>
-                    <CommentsForm slug={post.slug}/>
-                    <Comments slug={post.slug}/>
-
-                </div>
-
-                <div className='col-span-1'>
-                    <div className={'relative lg:sticky transition-all duration-300 '
-                        + (scrollDirection === 'up' || scrollY < 30 ?  ' lg:top-[100px]' : ' lg:top-[20px]')
-                    }>
-                        <PostWidget 
-                            slug={post.slug}
-                            categories={post.categories.map((category: any) => category.slug) }
-                            />
-                        <Categories />
-
+                        </div>
                     </div>
-                    
-                </div>
 
+                </div>
             </div>
-        </div>
+        </Layout>
     );
 }
 
