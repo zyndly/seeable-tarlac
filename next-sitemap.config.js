5422f8944c0dce7cad8639329ac32b45ad486a89
Zyndly <zyndlykentperlinbaja@gmail.com>
2022-09-13 22:23:37 +0800

    refactor: change to round

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,9 +3,8 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  // !STARTERCONF Change the siteUrl
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+
+  siteUrl: 'https://seeabletarlac.vercel.app/',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
