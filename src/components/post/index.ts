d7347fd2acc6a5818328f00e3cf06004742eeeda
Zyndly <zyndlykentperlinbaja@gmail.com>
2022-10-11 17:00:51 +0800

    refactor: move author components to post folder

--- src/components/post/index.ts
+++ src/components/post/index.ts
@@ -1,10 +1,10 @@
-export { default as Author } from './Author';
 export { default as Categories } from './Categories';
 export {default as CollectionsWidget} from './CollectionsWidget'; 
 export { default as Comments } from './comments/Comments';
 export { default as CommentsForm } from './comments/CommentsForm';
 export {default as FeaturedCollections} from './FeaturedCollections';
 export {default as Loader } from './Loader';
+export { default as Author } from './post/Author';
 export { default as PostCard } from './post/PostCard';
 export { default as PostDetail } from './post/PostDetail';
 export { default as PostWidget } from './post/PostWidget';
