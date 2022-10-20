087fee774822bbfacc4ee781c4adab5021ca360c
Zyndly <zyndlykentperlinbaja@gmail.com>
2022-10-20 13:57:29 +0800

    feat: initilize tarlac page

---
+++ src/pages/tarlac/tarlac.tsx
@@ -0,0 +1,26 @@
+import { Container } from '@/components/layout/Container';
+import Layout from "@/components/layout/Layout";
+import { TarlacPostCard } from "@/components/post/townWidget/two";
+import Seo from "@/components/Seo";
+import { PageHeading } from '@/components/text/PageHeading';
+
+
+export default function Tarlac() {
+  return (
+    <Layout>
+      <Seo 
+        templateTitle='Tarlac'
+        description=''
+      />
+
+      <main>
+      <PageHeading>Tarlac City</PageHeading>
+
+      <Container>
+        <TarlacPostCard />
+      </Container>
+      </main>
+
+    </Layout>
+  );
+}
