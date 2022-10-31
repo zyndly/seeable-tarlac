8f643ee397172f3068ccfabd64e80a14dafcbd96
Zyndly <zyndlykentperlinbaja@gmail.com>
2022-11-01 00:30:51 +0800

    feat: initial commit

---
+++ src/pages/tarlac/delicacies/index.tsx
@@ -0,0 +1,18 @@
+import Layout from "@/components/layout/Layout";
+import Template from "@/components/layout/Template";
+import Seo from "@/components/Seo";
+
+export default function DelicaciesPage() {
+  return (
+    <Layout>
+      <Seo 
+        templateTitle='Delicacies'
+        description=''
+      />
+
+      <Template/>
+
+      
+    </Layout>
+  );
+}
---
+++ src/pages/tarlac/festivals/index.tsx
@@ -0,0 +1,18 @@
+import Layout from "@/components/layout/Layout";
+import Template from "@/components/layout/Template";
+import Seo from "@/components/Seo";
+
+export default function FestivalsPage() {
+  return (
+    <Layout>
+      <Seo 
+        templateTitle='Festivals'
+        description=''
+      />
+
+      <Template/>
+
+      
+    </Layout>
+  );
+}
