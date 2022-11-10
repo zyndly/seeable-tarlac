512a64b159fb1c900b8e2f562b7b782ee49394f9
Zyndly <zyndlykentperlinbaja@gmail.com>
2022-11-10 23:13:26 +0800

    refactor(Container): change py-12 to py-4

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from 'react';
 
 export const Container = ({ children }: PropsWithChildren) => {
   return (
-    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12'>
-      {children}
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4'>
+        {children}
     </div>
   );
 };
