c01aa60cdc7a17b9aa5d8978fb875c1ae82eb5c7
Zyndly <zyndlykentperlinbaja@gmail.com>
2022-11-24 21:21:59 +0800

    add redirects to admin

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ module.exports = {
 
   reactStrictMode: true,
 
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://app.hygraph.com/4523b07f79a84b178e9662de3e0a97a1/master/content/6063f64fa3e34871b041ee251f0b9959/view/009bc1f6b3084fb38e2e781e4cac55e9',
+        permanent: false,
+      },
+    ]
+  },
+
   // media websites
   images: {
     domains: [
